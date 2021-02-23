560b8f1bdf14a072d4a8e13816828ed4144e4573
Andy Duenas <Duenas.g.andy@gmail.com>
2021-02-23 15:50:04 -0800

    Created a function that grabs random anime and displays them on the home page.#1

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 
 var $topRatedImgs = document.querySelectorAll('.top-rated');
 var $topRatedTitles = document.querySelectorAll('.anime-title-top');
-// var $fourRandomAnime = document.querySelectorAll('.anime-title');
+var $fourRandomTitle = document.querySelectorAll('.anime-title');
+var $fourRandomImgs = document.querySelectorAll('.four-rand-imgs');
 
 function getTopRated() {
   var xhr = new XMLHttpRequest();
@@ -20,17 +21,24 @@ function getTopRated() {
 
 getTopRated();
 
-function setTopRatedImgs(imgList) {
-  $topRatedImgs[0].setAttribute('src', imgList[0].image_url);
-  $topRatedImgs[1].setAttribute('src', imgList[1].image_url);
-  $topRatedImgs[2].setAttribute('src', imgList[2].image_url);
-  $topRatedTitles[0].textContent = imgList[0].title;
-  $topRatedTitles[1].textContent = imgList[1].title;
-  $topRatedTitles[2].textContent = imgList[2].title;
+function setTopRatedImgs(AnimeList) {
+  $topRatedImgs[0].setAttribute('src', AnimeList[0].image_url);
+  $topRatedImgs[1].setAttribute('src', AnimeList[1].image_url);
+  $topRatedImgs[2].setAttribute('src', AnimeList[2].image_url);
+  $topRatedTitles[0].textContent = AnimeList[0].title;
+  $topRatedTitles[1].textContent = AnimeList[1].title;
+  $topRatedTitles[2].textContent = AnimeList[2].title;
 }
 
-function setFourRandomAnime() {
-
+function setFourRandomAnime(AnimeList) {
+  $fourRandomImgs[0].setAttribute('src', AnimeList[0].image_url);
+  $fourRandomImgs[1].setAttribute('src', AnimeList[1].image_url);
+  $fourRandomImgs[2].setAttribute('src', AnimeList[2].image_url);
+  $fourRandomImgs[3].setAttribute('src', AnimeList[3].image_url);
+  $fourRandomTitle[0].textContent = AnimeList[0].title;
+  $fourRandomTitle[1].textContent = AnimeList[1].title;
+  $fourRandomTitle[2].textContent = AnimeList[2].title;
+  $fourRandomTitle[3].textContent = AnimeList[3].title;
 }
 
 function setRandomAnime(animeList, numOfAnime) {
